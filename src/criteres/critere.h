9778a5d0b0ffcd7940319fa790d1347fa7a6b6dd
Tino Dolbeau <tinodolbeau@gmail.com~>
2025-11-04 10:36:24 +0100

    ajout critere par default

--- src/criteres/critere.h
+++ src/criteres/critere.h
@@ -5,12 +5,14 @@
 class Critere {
     private:
     public:
-    
+
         /**
          * @brief Compares two rules according to the criterion
          * @param r1 the first rule
          * @param r2 the second rule
          * @return true if r2 is better than r1 according to the criterion, false otherwise
          */
-        virtual bool betterThan(std::shared_ptr<Rule> r1, std::shared_ptr<Rule> r2) = 0;
+        virtual bool betterThan(std::shared_ptr<Rule> r1, std::shared_ptr<Rule> r2) {
+            return false;
+        };
 };
