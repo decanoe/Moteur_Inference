342f2c93d4c3eb29ee1a0311e7c02fbd23fae20e
Tino Dolbeau <tinodolbeau@gmail.com~>
2025-11-04 11:40:23 +0100

    skip useless rules

--- src/algos/forwardChaining.h
+++ src/algos/forwardChaining.h
@@ -8,7 +8,7 @@ public:
 
     void run(RuleBase &rule_base, FactBase &fact_base, std::shared_ptr<Fact> goal) override
     {
-        std::cout << "Starting solving with forward chaining algorithm and "+(*critere).toString()+"\n";
+        std::cout << "Starting solving with forward chaining algorithm and " + (*critere).toString() + "\n";
         std::vector<std::shared_ptr<Rule>> rules = rule_base.get_rules();
         bool newFact = false;
         do
@@ -16,16 +16,32 @@ public:
             std::cout << "Starting new iteration of forward chaining...\n";
             newFact = false;
             std::vector<std::shared_ptr<Rule>>::iterator rule_to_use = rules.end();
-            for (auto it_rules = std::next(rules.begin()); it_rules != rules.end(); it_rules++)
+            for (auto it_rules = rules.begin(); it_rules != rules.end();)
             {
-                if ((*it_rules)->ruleValidated(fact_base))
+                bool uselessRule = true;
+                for (auto fact : (*it_rules)->get_consequents())
                 {
-                    if (rule_to_use == rules.end() || critere->betterThan(*it_rules, *rule_to_use, fact_base))
+                    if (!fact_base.contains_fact(fact))
                     {
-                        rule_to_use = it_rules;
-                        std::cout << "Nouvelle meilleure règle: " << **rule_to_use << "\n";
+                        uselessRule = false;
+                        break;
                     }
                 }
+                if (uselessRule)
+                {
+                    std::cout << "Skipping useless rule: " << **it_rules << "\n";
+                    it_rules = rules.erase(it_rules);
+                    continue;
+                }
+                if ((*it_rules)->ruleValidated(fact_base))
+                {
+                    if (rule_to_use >= rules.end() || critere->betterThan(*it_rules, *rule_to_use, fact_base))
+                    {
+                        rule_to_use = it_rules;
+                        std::cout << "Nouvelle meilleure règle: " << *rule_to_use << "\n";
+                    }
+                }
+                ++it_rules;
             }
             if (rule_to_use == rules.end())
             {
@@ -35,7 +51,8 @@ public:
             if ((*rule_to_use)->update_fact_base(fact_base))
             {
                 std::cout << "Applied rule: " << *rule_to_use << ", new facts inferred.\n";
-                std::cout << "Current fact base: \n" << fact_base << "\n";
+                std::cout << "Current fact base: \n"
+                          << fact_base << "\n";
                 if (goal != nullptr && (*rule_to_use)->contains_consequent(goal))
                 {
                     std::cout << "Goal fact found: " << goal << "\n";
@@ -45,5 +62,5 @@ public:
                 rules.erase(rule_to_use);
             }
         } while (!rules.empty() && newFact);
-    } 
+    }
 };
